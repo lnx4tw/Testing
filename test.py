28134c37518bdf2a677905bec4cb09f0c81fb1b2
lnx4tw <lnx4tw@gmail.com>
2019-01-04 00:35:57 -0500

    added date/time code to test.py

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 ##comment about my super cool script
+from datetime import datetime
+
+now = datetime.now()
+
+mm = str(now.month)
+
+dd = str(now.day)
+
+yyyy = str(now.year)
+
+hour = str(now.hour)
+
+mi = str(now.minute)
+
+ss = str(now.second)
+
+print (mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
